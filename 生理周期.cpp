ab16d861f05779d0dbe8a79d635773007db9578f
mingren1317 <www.2674672641@qq.com>
2021-02-25 17:32:39 +0800

    提交代码为了演示GitHub

---
+++ 生理周期.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//int main()
+//{
+//	int p, e, i, d;
+//	cin >> p >> e >> i >> d;
+//	for (int k = d+1; k <= 21252; k++)
+//	{
+//		if ((k - p) % 23 == 0 && (k - e) % 28 == 0 && (k - i) % 33 == 0)
+//			cout << k-d << endl;
+//	}
+//}
+
+//ÓÅ»¯
+
+//int main()
+//{
+//	int p,e,i, d;
+//	cin >> p >> e >> i >> d;
+//	int k;
+//	for (k = d + 1; (k - p) % 23; k++);
+//	for (; (k - e) % 28; k+=23);
+//	for (; (k - i) % 33; k+=23*28);
+//	cout << k - d;
+//}
